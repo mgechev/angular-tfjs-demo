512cac213c1e74aec4ad9c936812afa552917c46
mgechev <mgechev@gmail.com>
2020-10-15 03:36:44 +0300

    Add complete page navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
+  },
+  {
+    path: 'about',
+    loadChildren: () =>
+      import('./about/about.module').then((m) => m.AboutModule),
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
-import { filter, map } from 'rxjs/operators';
+import { Router } from '@angular/router';
+import { filter, map, withLatestFrom } from 'rxjs/operators';
 import { HandGesture } from './hand-gesture.service';
 
 @Component({
@@ -10,19 +11,30 @@ import { HandGesture } from './hand-gesture.service';
 export class AppComponent implements AfterViewInit {
   @ViewChild('video') video: ElementRef<HTMLVideoElement>;
   @ViewChild('canvas') canvas: ElementRef<HTMLCanvasElement>;
+  @ViewChild('home') homeLink: ElementRef<HTMLAnchorElement>;
+  @ViewChild('about') aboutLink: ElementRef<HTMLAnchorElement>;
+
   opened$ = this._recognizer.swipe$.pipe(
     filter((value) => value === 'left' || value === 'right'),
     map((value) => value === 'right')
   );
 
-  constructor(private _recognizer: HandGesture) {
+  selection$ = this._recognizer.gesture$.pipe(
+    filter((value) => value === 'one' || value === 'two'),
+    map((value) => (value === 'one' ? 'home' : 'about'))
+  );
+
+  constructor(private _recognizer: HandGesture, private _router: Router) {
     this._recognizer.swipe$.subscribe((d) => {
       console.log(d);
     });
 
-    this._recognizer.gesture$.subscribe((d) => {
-      console.log(d);
-    });
+    this._recognizer.gesture$
+      .pipe(
+        filter((value) => value === 'ok'),
+        withLatestFrom(this.selection$)
+      )
+      .subscribe(([_, page]) => this._router.navigateByUrl(page));
   }
 
   get stream(): MediaStream {
